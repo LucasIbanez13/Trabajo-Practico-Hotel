6483af8c5351de8590e9ce3cf0ef1dee562c801f
Peraltavictoria <peraltavictoria239@gmail.com>
2026-05-19 17:56:50 -0300

    agrego cors y getReservas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
+import cors from "cors";
 import reservasRoutes from "./routes/reservas.routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api", reservasRoutes);
-
 
 app.listen(3000);
 console.log("Server on port", 3000);
