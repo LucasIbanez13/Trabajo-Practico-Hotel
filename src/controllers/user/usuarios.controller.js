1b2de0a32772975b18be5433edc18f2b9f43dce6
dakodev <123116563+dakoderdev@users.noreply.github.com>
2026-06-15 19:04:15 -0300

    backend editar/eliminar usuario hecho

--- src/controllers/user/usuarios.controller.js
+++ src/controllers/user/usuarios.controller.js
@@ -16,3 +16,50 @@ export const getUsuarios = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editarUsuario = async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+
+    const { nombre, email, rol } = req.body;
+
+    const usuario = await prisma.usuario.update({
+      where: { id },
+      data: {
+        nombre,
+        email,
+        rol,
+      },
+      select: {
+        id: true,
+        nombre: true,
+        email: true,
+        rol: true,
+        createdAt: true,
+      },
+    });
+
+    res.json(usuario);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const eliminarUsuario = async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+
+    await prisma.usuario.delete({
+      where: { id },
+    });
+
+    res.json({
+      message: "Usuario eliminado correctamente",
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import validateToken from "../auth/verifyToken.js";
 import { checkRole } from "../middlewares/checkRole.js";
-import { getUsuarios } from "../controllers/user/usuarios.controller.js";
+import { getUsuarios, editarUsuario, eliminarUsuario } from "../controllers/user/usuarios.controller.js";
 
 const router = Router();
 
 router.get("/usuarios", validateToken, checkRole("superadmin"), getUsuarios);
+router.put("/usuarios/:id", validateToken, checkRole("superadmin"), editarUsuario);
+router.delete("/usuarios/:id", validateToken, checkRole("superadmin"), eliminarUsuario);
 
 export default router;
