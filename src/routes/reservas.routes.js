12e3128fc75001a8e6de7da5ba22b5551400c1a6
lucilapastranaok-hue <lucilapastrana.ok@gmail.com>
2026-06-15 18:11:44 -0300

    subo middleware para proteger rutas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -2,17 +2,16 @@ import { Router } from "express";
 import { getReservas } from "../controllers/reservas.controller.js";
 import { createReserva } from "../controllers/reservasCreate.controller.js";
 import { deleteReserva } from "../controllers/reservasDelete.controller.js";
-import { getObtenerID } from "../controllers/reservasId.controller.js"; 
-import { updateReserva } from "../controllers/reservasUpdate.controller.js"; 
-
+import { getObtenerID } from "../controllers/reservasId.controller.js";
+import { updateReserva } from "../controllers/reservasUpdate.controller.js";
+import validateToken from "../auth/verifyToken.js";
 
 const router = Router();
 
-router.get("/reservas", getReservas);
-router.get("/reservas/:id", getObtenerID)
-router.post("/reservas", createReserva);
-router.delete("/reservas/:id", deleteReserva);
-router.put("/reservas/:id", updateReserva);
-
+router.get("/reservas", validateToken, getReservas);
+router.get("/reservas/:id", validateToken, getObtenerID);
+router.post("/reservas", validateToken, createReserva);
+router.delete("/reservas/:id", validateToken, deleteReserva);
+router.put("/reservas/:id", validateToken, updateReserva);
 
 export default router;
